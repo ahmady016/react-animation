a71e583bc087df5ba1d40a2635d68e380f1c2b79
ahmady016 <ahmady09@gmail.com>
2019-01-14 22:56:22 +0200

    Add pulse animation and make Box animation setting, styling from props

--- src/App.js
+++ src/App.js
@@ -2,19 +2,55 @@ import React, { Component } from 'react';
 import Box from './Box';
 import './app.css';
 
+const topBottom = {
+  top: {
+    y: 20,
+    scale: 1,
+    backgroundColor: "#428cb2"
+  },
+  bottom: {
+    y: 360,
+    scale: 2,
+    backgroundColor: "#396e94"
+  }
+}
+
+const pulse = {
+  off: {
+    opacity: 0.5,
+    scale: 1
+  },
+  on: {
+    opacity: 1,
+    scale: 2,
+    transition: {
+      type: "spring",
+      stiffness: 500,
+      delay: 200
+    }
+  }
+};
+
 class App extends Component {
   state = {
-    pos: 'top'
+    pos: 'top',
+    pulse: 'off'
   }
   componentDidMount() {
-    this.timer = setInterval( () => this.setState({ pos: (Math.random() <= 0.5 ? 'top' : 'bottom' )}), 1000);
+    this.timer = setInterval(() => this.setState({
+          pos:    Math.random() <= 0.5 ? "top" : "bottom",
+          pulse:  Math.random() <= 0.5 ? "off" : "on"
+        }), 1000);
   }
   componentWillUnmount() {
     clearInterval(this.timer);
   }
   render() {
     return (
-      <Box size={50} pose={this.state.pos} />
+      <>
+        <Box size={50}  offset="50%"          pose={this.state.pos}   config={topBottom} />
+        <Box size={100} offset="25%" top={50} pose={this.state.pulse} config={pulse} />
+      </>
     );
   }
 }
--- src/Box.js
+++ src/Box.js
@@ -1,25 +1,15 @@
 import posed from "react-pose";
 import styled from "styled-components";
 
-const box = posed.div({
-  top: {
-    y: 20,
-    scale: 1,
-    background: "#428cb2"
-  },
-  bottom: {
-    y: 360,
-    scale: 2,
-    background: "#396e94"
-  }
-});
+const box = posed.div( ({ config }) => config );
 
 const Box = styled(box)`
   position: absolute;
-  ${props => `
+  ${ props => `
     height: ${props.size}px;
     width: ${props.size}px;
-    left: calc(50% - ${props.size / 2}px);
+    top: ${props.top || 0}px;
+    left: calc(${props.offset} - ${props.size / 2}px);
   `}
 `;
 
